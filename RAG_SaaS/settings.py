fbff88c6e8f5d3cf9dd33d1458d8f4170b0830f4
Esteban Caicedo <esteban.caicedo@vml.com>
2025-04-11 13:54:10 -0500

    Update settings.py

--- RAG_SaaS/settings.py
+++ RAG_SaaS/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-nf&e1vvw_dmvp%9ky+j#g+eryuo*($db=^4(k%)q(d4sgxk4+q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.60.49.219', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['34.60.49.219', 'localhost', '127.0.0.1','34.57.126.1']
 
 
 
@@ -64,13 +64,15 @@ CORS_ALLOW_CREDENTIALS = True  # Enable credentials
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "http://34.60.49.219:8000"
+    "http://34.60.49.219:8000",
+    "http://34.57.126.1:8000"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "http://34.60.49.219:8000"
+    "http://34.60.49.219:8000",
+    "http://34.57.126.1:8000"
 ]
 
 
